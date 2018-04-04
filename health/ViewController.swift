d07e3b512abf5bccddbf2d07bd1689d0f56e1a31
guangwei li <guangweili@guangweidemacbook-pro.local>
2018-04-04 12:50:45 +0800

    Initial Commit

---
+++ health/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  health
+//
+//  Created by guangwei li on 2018/4/4.
+//  Copyright © 2018年 guangwei li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
